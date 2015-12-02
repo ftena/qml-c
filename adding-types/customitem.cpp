ea138d6d02439ee83d7c707f3c24b04cecb89aac
Tarod <tarod.net@gmail.com>
2015-12-02 14:32:28 +0100

    more info

--- adding-types/customitem.cpp
+++ adding-types/customitem.cpp
@@ -14,6 +14,15 @@ int CustomItem::size() const
 void CustomItem::setSize(int size)
 {
     m_size = size;
+
+    /*To prevent loops or excessive evaluation, developers should ensure that
+     * the property change signal is only emitted when the property value has
+     * actually changed.
+     * Also, if a property or group of properties is infrequently used,
+     * it is permitted to use the same NOTIFY signal for several properties.
+     * This should be done with care to ensure that performance doesn't
+     * suffer.
+    */
     emit sizeChanged(m_size);
 }
 
